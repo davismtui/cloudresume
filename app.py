febda33155eca0a84f1a33abe8cb1353d61fa074
davismtui <dmtui2@uwo.ca>
2023-10-10 16:27:01 -0400

    fixed email

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
-
+import updater
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return render_template('index.html')
+    number = updater.update()
+    return render_template('index.html', number=f'Page View Count: {number}')
+
+
 
 
 if __name__ == '__main__':
